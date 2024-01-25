bf758406ce4db2c60f3514ef6b8ae0e787a546c7
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-01-25 18:20:59 +0100

    random_naam debug poging

--- faker/functies.py
+++ faker/functies.py
@@ -4,4 +4,6 @@ def random_naam():
     with open('faker/namen.txt','r') as file:
         namen_lijst = file.read()
         list(namen_lijst)
+        print(type(namen_lijst))
     return namen_lijst
+print(random_naam())
