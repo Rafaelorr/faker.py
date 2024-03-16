f5245ed6cd3fff58b6b34400e73e953199ec6493
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-16 17:00:34 +0100

    voeg functie namen toe aan functies.py

--- faker/functies.py
+++ faker/functies.py
@@ -4,3 +4,42 @@ from namen import namenlijst_alle_geslachten
 def random_naam():
     namen_lijst:list = namenlijst_alle_geslachten
     return choice(namen_lijst)
+
+def random_telefoon_nummer():
+    pass
+
+def random_datum():
+    pass
+
+def random_zipcode():
+    pass
+
+def random_stad():
+    pass
+
+def random_straat():
+    pass
+
+def random_adress():
+    pass
+
+def random_email():
+    pass
+
+def random_Username():
+    pass
+
+def random_top_level_domain():
+    pass
+
+def random_domain():
+    pass
+
+def random_companyName():
+    pass
+
+def random_companySuffix():
+    pass
+
+def random_slogan():
+    pass
