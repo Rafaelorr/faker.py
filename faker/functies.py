e4baf7c80db0b4ed25831f47934fb2bb3036462f
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-16 10:09:41 +0100

    voeg types toe aan random_naam functie

--- faker/functies.py
+++ faker/functies.py
@@ -2,5 +2,5 @@ from random import choice
 from namen import namenlijst_alle_geslachten
 
 def random_naam():
-    namen_lijst = namenlijst_alle_geslachten
+    namen_lijst:list = namenlijst_alle_geslachten
     return choice(namen_lijst)
