118a0dae172a9674584eab4129a7791221ff8cfa
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-03-17 19:57:37 +0100

    maak random_telefoon_nummer functie zonder data

--- faker/random_telefoon_nummer.py
+++ faker/random_telefoon_nummer.py
@@ -1,4 +1,6 @@
-from random import randint
+from random import choice
 
 def random_telefoon_nummer():
-    pass
+    telefoon_nummer_lijst:list = []
+    telefoon_nummer = choice(telefoon_nummer_lijst)
+    return telefoon_nummer
