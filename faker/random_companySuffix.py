7f8e7cea0efe3040be9a33aad6c139b4b02475ce
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-12-30 20:19:46 +0100

    voeg alle Amerikaanse company suffixs toe

--- faker/random_companySuffix.py
+++ faker/random_companySuffix.py
@@ -1,6 +1,6 @@
 from random import choice
 
 def random_companySuffix():
-    companySuffix_list = []
+    companySuffix_list = ["Ltd","Ltd.","S.A.","SA","A.G.","AG","N.V.","NV","Ltee","B.V.","BV","GmbH","L.L.C.","LLC","SIA","Sia","Inc.","Inc","Corp.","Corp","Pte. Ltd"]
     companySuffix = choice(companySuffix_list)
     return companySuffix
