1a48d6b7d24dfea3ddcec4f979562a3453b78b91
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-01-04 18:11:53 +0100

    maak de randon_datum functie af

--- faker/random_datum.py
+++ faker/random_datum.py
@@ -1,4 +1,24 @@
-from random import randint
+import random
+import time
+    
+def str_time_prop(start, end, time_format, prop):
+    """
+    Deze functie genereert een random datum tussen twee datums.
+    Die twee datums moeten in een speficieke format zijn..
+    Voorbeeld van dat format: 1/1/2008 1:30 PM
 
-def random_datum():
-    pass
+    Returns:
+       string: de datum
+    """
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y %I:%M %p', prop)
+    
+print(random_date("1/1/2008 1:30 PM", "1/1/2030 12:50 PM", random.random()))
