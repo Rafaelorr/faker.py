b2e20235ad6e5974a0ae42f52f59f51779ec7c69
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-01-12 14:58:22 +0100

    maak random_email functie af

--- faker/random_email.py
+++ faker/random_email.py
@@ -1,4 +1,8 @@
-from random import choice
+from random_domain import random_domain
+from random_top_level_domain import random_top_level_domain
 
-def random_email():
-    pass
+def random_email() -> str:
+    domain: str = random_domain()
+    tld: str = random_top_level_domain()
+    email: str = domain + "." + tld
+    return email
