9e1181be9aec3dd258bbe47a20455acaddc38dd9
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-01-12 14:57:43 +0100

    voeg email domains toe aan random_domain functie

--- faker/random_domain.py
+++ faker/random_domain.py
@@ -1,6 +1,6 @@
 from random import choice
 
 def random_domain() -> str:
-    domain_naam_lijst: list[str] = []
+    domain_naam_lijst: list[str] = ["gmail","yahoo","hotmail","yahoo.co","hotmail.co","outlook",]
     domain_naam: str = choice(domain_naam_lijst)
     return domain_naam
