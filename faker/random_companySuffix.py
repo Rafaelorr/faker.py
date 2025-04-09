5d8846dc71abd3ebeaadcb143802f1c59c0f58a2
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-04-09 20:03:06 +0200

    formate alle lijsten voor een item per new line

--- faker/random_companySuffix.py
+++ faker/random_companySuffix.py
@@ -1,6 +1,26 @@
 from random import choice
 
 def random_companySuffix() -> str:
-    companySuffix_list: list[str] = ["Ltd","Ltd.","S.A.","SA","A.G.","AG","N.V.","NV","Ltee","B.V.","BV","GmbH","L.L.C.","LLC","SIA","Sia","Inc.","Inc","Corp.","Corp","Pte. Ltd"]
+    companySuffix_list: list[str] = ["Ltd",
+"Ltd.",
+"S.A.",
+"SA",
+"A.G.",
+"AG",
+"N.V.",
+"NV",
+"Ltee",
+"B.V.",
+"BV",
+"GmbH",
+"L.L.C.",
+"LLC",
+"SIA",
+"Sia",
+"Inc.",
+"Inc",
+"Corp.",
+"Corp",
+"Pte. Ltd"]
     companySuffix: str = choice(companySuffix_list)
     return companySuffix
--- faker/random_domain.py
+++ faker/random_domain.py
@@ -1,6 +1,11 @@
 from random import choice
 
 def random_domain() -> str:
-    domain_naam_lijst: list[str] = ["gmail","yahoo","hotmail","yahoo.co","hotmail.co","outlook",]
+    domain_naam_lijst: list[str] = ["gmail",
+"yahoo",
+"hotmail",
+"yahoo.co",
+"hotmail.co",
+"outlook",]
     domain_naam: str = choice(domain_naam_lijst)
     return domain_naam
