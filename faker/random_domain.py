5e81d7bd2355630ba39fc2c09e38a16a926c92bb
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-04-11 18:00:03 +0200

    verplaats de domain_naam_lijst naar een aparte file

--- faker/random_domain.py
+++ faker/random_domain.py
@@ -1,11 +1,6 @@
 from random import choice
+from domain_namen import domain_naam_lijst
 
 def random_domain() -> str:
-    domain_naam_lijst: list[str] = ["gmail",
-"yahoo",
-"hotmail",
-"yahoo.co",
-"hotmail.co",
-"outlook",]
     domain_naam: str = choice(domain_naam_lijst)
     return domain_naam
