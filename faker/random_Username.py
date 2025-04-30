bff062f63aa91e18058287c938b5f611b7bdfa0e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-04-30 13:26:42 +0200

    verplaats username_lijst naar de lijsten folder

--- faker/random_Username.py
+++ faker/random_Username.py
@@ -1,5 +1,5 @@
 from random import choice
-from username_lijst import username_lijst
+from faker.lijsten.username_lijst import username_lijst
 
 def random_Username() -> str:
     return choice(username_lijst)
