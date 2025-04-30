54d8b504327b46c8af54af710ffece11d3aead8c
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-04-30 13:35:24 +0200

    verplaats us_counties en us_staten naar de lijsten folder

--- faker/random_us_adress.py
+++ faker/random_us_adress.py
@@ -1,6 +1,6 @@
 from random import randint, choice
-from us_counties import us_counties
-from us_staten import us_staten
+from faker.lijsten.us_counties import us_counties
+from faker.lijsten.us_staten import us_staten
 
 def random_adress(max_adress:int) -> str:
     adress_nummer:int = randint(1,max_adress)
