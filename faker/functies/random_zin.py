e8d8cebb9a6fa168abc3805fc01b5e35c5d891a1
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-08-14 11:42:19 +0200

    voeg random_zin functie toe

---
+++ faker/functies/random_zin.py
@@ -0,0 +1,18 @@
+from random import choice, randint
+from lijsten.nl_lorem import nl_woorden_lijst
+from lijsten.us_lorem import us_word_list
+
+def random_zin(lijst=None, taal="nl") -> str:
+    if lijst is None:
+        if taal.lower() == "nl":
+            lijst = nl_woorden_lijst
+        elif taal.lower() == "us":
+            lijst = us_word_list
+        else:
+            raise ValueError("Invalid language entered")
+
+    zinwoorden = [choice(lijst) for _ in range(randint(1, len(lijst)))]
+    zin = ''.join(zinwoorden) + '.'
+    return zin
+
+print(random_zin())
